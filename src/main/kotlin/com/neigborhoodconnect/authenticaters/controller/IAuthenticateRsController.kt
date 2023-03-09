8b1e5c8cfc7e320e72784cc1f84b339722949073
jimmy <tarunpreetsingh16@gmail.com>
2023-03-08 19:50:29 -0500

    testing branch rule

--- src/main/kotlin/com/neigborhoodconnect/authenticaters/controller/IAuthenticateRsController.kt
+++ src/main/kotlin/com/neigborhoodconnect/authenticaters/controller/IAuthenticateRsController.kt
@@ -1,9 +1,14 @@
 package com.neigborhoodconnect.authenticaters.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 interface IAuthenticateRsController {
 
     @GetMapping("/heartbeat")
     fun heartBeat(): String
+
+    @PostMapping("/sign-up")
+    fun signUp(): ResponseEntity<String>
 }
