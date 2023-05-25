dcd22f017f1b6afe4c0435d0c4e93828e3e1634d
jimmy <tarunpreetsingh16@gmail.com>
2023-05-24 22:07:55 -0400

    updated entity

--- src/main/kotlin/com/neighborhood/connect/authenticate/entities/UserCredentials.kt
+++ src/main/kotlin/com/neighborhood/connect/authenticate/entities/UserCredentials.kt
@@ -1,23 +1,22 @@
 package com.neighborhood.connect.authenticate.entities
 
 import jakarta.persistence.*
+import lombok.Data
 
 @Entity
+@Data
 @Table(name = "user_credentials")
 data class UserCredentials (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    val id: Int? = null,
 
     @Column(name = "username", nullable = false)
     val username: String?,
 
     @Column(name = "password", nullable = false)
     val password: String?,
-
-    @Column(name = "access_token", nullable = true, )
-    val accessToken: String? = null
 ) {
-    constructor() : this(null, null, null, null)
+    constructor() : this(null, null, null)
 }
